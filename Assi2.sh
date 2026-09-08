196000b73e66d8fa2e31d50ba071de1978324078
romitparmar1 <romitparmar111@gmail.com>
2026-09-08 22:00:18 +0530

    Update Assi2.sh

--- Assi2.sh
+++ Assi2.sh
@@ -5,7 +5,7 @@ read -p "Enter The Number = " number2
 read -p "Enter The Number = " number3
 
 if [ $number1 -gt $number2 ] && [ $number1 -gt $number3 ]; then
-	echo "number1 is Largest among three"
+	echo "$number1 is Largest among three"
 elif [ $number2 -gt $number1 ] && [ $number2 -gt $number3 ]; then
 	echo "$number2 is largest among three"
 else
